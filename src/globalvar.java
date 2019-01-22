70b671d6bb06a67bc4b9ab329d071129f93b8f63
Chaitanya <1hollowpumpkin1@gmail.com>
2019-01-22 11:02:57 +0530

    First commit

---
+++ src/globalvar.java
@@ -0,0 +1,21 @@
+
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Nilkanth
+ */
+public class globalvar {
+    //ALL GLOBAL VARIABLES ARE DEFINED HERE
+public static String year;
+public static EM_Main frame1;
+public static String tablename;
+public static Day frame2;
+public static Earnings frame3;
+public static String isMonthlyExpensesBeingShown = "No";
+public static String day;
+}
